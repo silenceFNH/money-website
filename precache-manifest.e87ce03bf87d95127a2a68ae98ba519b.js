64b079624e48940a06b79d4d4430d43e5c68f27d
silence <867943221@qq.com>
2021-07-14 18:24:36 +0800

    deploy

---
+++ precache-manifest.e87ce03bf87d95127a2a68ae98ba519b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a896d1949cbb880966d5",
+    "url": "/money-website/css/app.d1c0f8a8.css"
+  },
+  {
+    "revision": "47325ada9e6bc933a5fbf921efc8354f",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "a896d1949cbb880966d5",
+    "url": "/money-website/js/app.1b4a9189.js"
+  },
+  {
+    "revision": "7406bafd0cc9e20fdc4a",
+    "url": "/money-website/js/chunk-vendors.651caffe.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "e1725d7d3f3adc53df6e472aa9a58049",
+    "url": "/money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
